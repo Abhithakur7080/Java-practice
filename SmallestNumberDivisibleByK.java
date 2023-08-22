50971b2edaf5c959943d2dbc3d74b05737c49a44
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2023-08-22 22:14:14 +0530

    add recursion

--- SmallestNumberDivisibleByK.java
+++ SmallestNumberDivisibleByK.java
@@ -9,8 +9,17 @@ public class SmallestNumberDivisibleByK {
         }
         return -1;
     }
+    public static int lengthOfNumber1(int k){
+        return helper(1, 1, k);
+    }
+    public static int helper(int num, int length, int k){
+        if(num%k == 0) return length;
+        int newNum = (num*10 + 1)%k;
+        if(newNum == 1) return -1;
+        return helper(newNum, length+1, k);
+    }
     public static void main(String[] args) {
         int k = scanner.s.nextInt();
-        System.out.println(lengthOfNumber(k));
+        System.out.println(lengthOfNumber1(k));
     }
 }
